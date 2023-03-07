48a1ea45eddc827c0801a86e7b5c9ec429668d10
unknown <rahma.sakti24@gmail.com>
2023-03-07 17:40:43 +0700

    merapikan

--- modul3_1302210095/Form1.cs
+++ modul3_1302210095/Form1.cs
@@ -5,9 +5,9 @@ namespace modul3_1302210095
         int[] numArr = new int[100];
         char[] oprationArr = new char[100];
 
-        int num = 0;
+        string num = "0";
 
-        int num1, num2;
+        string num1, num2;
 
         bool submit = true;
         public Form1()
@@ -30,77 +30,79 @@ namespace modul3_1302210095
         private void button1_Click(object sender, EventArgs e)
         {
             change_label(label1, button1.Text);
-            num = 1;
+            num += "1";
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             change_label(label1, button2.Text);
-            num = 2;
+            num += "2";
             
         }
         private void button3_Click(object sender, EventArgs e)
         {
             change_label(label1, button3.Text);
-            num = 3;
+            num += "3";
 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             change_label(label1, button4.Text);
-            num = 4;
+            num += "4";
 
         }
         private void button5_Click(object sender, EventArgs e)
         {
             change_label(label1, button5.Text);
-            num = 5;
+            num += "5";
            
         }
         private void button6_Click(object sender, EventArgs e)
         {
             change_label(label1, button6.Text);
-            num = 6;
+            num += "6";
             
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             change_label(label1, button7.Text);
-            num = 7;
+            num += "7";
         }
         private void button8_Click(object sender, EventArgs e)
         {
             change_label(label1, button8.Text);
-            num = 8;
+            num += "8";
 
         }
         private void button9_Click(object sender, EventArgs e)
         {
             change_label(label1, button9.Text);
-            num = 9;
+            num += "9";
 
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
             change_label(label1, button10.Text);
-            num = num * 10;
+            num += "0";
 
         }
         private void button11_Click(object sender, EventArgs e)
         {
             change_label(label1, button11.Text);
             num1 = num;
+            num = "";
 
         }
         private void button12_Click(object sender, EventArgs e)
         {
             label1.Text = "";
-            change_label(label1, Convert.ToString(num+num1));
-            submit = true;
+            int bil1 = Convert.ToInt32(num);
+            int bil2 = Convert.ToInt32(num1);
+            change_label(label1, Convert.ToString( bil1 + bil2));
         }
         private void label1_Click(object sender, EventArgs e)
         {
